50523ff451eeae4df1ca6ec52f80d2f8de490d6f
Fatima-git10 <tahirawan11@gmail.com>
2026-09-15 17:19:39 +0200

    Add files via upload

---
+++ Function/function.py
@@ -0,0 +1,14 @@
+# Normal Function
+def greeting():
+    print("hello Fatima")
+greeting()
+
+# Function with return statement
+def question():
+    return "how are you"
+print(question())
+
+# function with parameter and return statement
+def add(a,b):
+    return a+b
+print (add(10,20))
